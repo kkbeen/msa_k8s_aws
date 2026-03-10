97b15629294b4c57f92d7f0fa45e6cd5f09d43c0
kkbeen <kkbeen@naver.com>
2026-03-10 13:39:20 +0900

    2nd commit

--- user/src/test/java/com/example/user/UserApplicationTests.java
+++ user/src/test/java/com/example/user/UserApplicationTests.java
@@ -1,12 +1,12 @@
 package com.example.user;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+// @SpringBootTest
 class UserApplicationTests {
 
-	@Test
+	// @Test
 	void contextLoads() {
 	}
 
